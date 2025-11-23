805259f1c85dbaa6cb6325c4fc5231f1475b2727
ClauuGithub <164614753+ClauuGithub@users.noreply.github.com>
2025-11-23 20:06:10 +0100

    GameHeader añadido

---
+++ Assets/Scripts/Game Scene/ButtonsColorController.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+
+// It's not workiiiing
+public class ButtonsColorController : MonoBehaviour
+{
+    [Header("Botones")]
+    public Button doButton;
+    public Button faButton;
+    public Button solButton;
+
+    [Header("Backgrounds")]
+    public Image doBackground;
+    public Image faBackground;
+    public Image solBackground;
+
+    [Header("Colores")]
+    public Color clickedColor = Color.green;
+
+    void Awake()
+    {
+        doButton.onClick.AddListener(() => ChangeBackground(doBackground));
+        faButton.onClick.AddListener(() => ChangeBackground(faBackground));
+        solButton.onClick.AddListener(() => ChangeBackground(solBackground));
+    }
+
+    void ChangeBackground(Image background)
+    {
+        background.color = clickedColor;
+    }
+}
