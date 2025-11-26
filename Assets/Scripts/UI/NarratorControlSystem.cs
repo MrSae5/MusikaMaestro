28d8ab331f350e4d31aa55b30e8e56f541e6c9a6
ClauuGithub <164614753+ClauuGithub@users.noreply.github.com>
2025-11-26 21:29:31 +0100

    Grey Mask aplicadas

--- Assets/Scripts/UI/NarratorControlSystem.cs
+++ Assets/Scripts/UI/NarratorControlSystem.cs
@@ -11,23 +11,19 @@ public class NarratorControlSystem : MonoBehaviour
     // Llama esto cuando cambias de pantalla
     public void ShowScreen(int index)
     {
-        // Solo activa la pantalla que quieres mostrar
         screens[index].SetActive(true);
 
-        // Recorre todos los paneles para manejar los audios
         for (int i = 0; i < screens.Length; i++)
         {
             bool isActive = screens[i].activeSelf;
 
             if (toggle.isOn && i == index)
             {
-                // Reproduce el audio del panel actual si el toggle está activado
                 if (!screenAudios[i].isPlaying)
                     screenAudios[i].Play();
             }
             else
             {
-                // Detiene el audio de los otros paneles
                 if (screenAudios[i].isPlaying)
                     screenAudios[i].Stop();
             }
