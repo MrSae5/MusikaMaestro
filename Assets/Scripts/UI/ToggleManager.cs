c747d3442a71065baf11910df446c5ad71df2341
Sara <samepae01@gmail.com>
2025-11-26 17:51:34 +0100

    Narrador
    - Warning auto siempre
    - Menú depende de botón (el main la primera vez no suena aún por q no funciona por que no da a ningún botón para cambiar de pantalla, si da tiempo se arregla)
    - Falta en la gameScene y como funcionaría

--- Assets/Scripts/UI/ToggleManager.cs
+++ Assets/Scripts/UI/ToggleManager.cs
@@ -6,9 +6,12 @@ public class ToggleManager : MonoBehaviour
     [SerializeField] private Toggle _toggle;
     [SerializeField] private Animator _animator;
 
+    public bool IsOn => _toggle.isOn;
+
     private void Start()
     {
         UpdateAnimator();
+        _toggle.onValueChanged.AddListener(delegate { UpdateAnimator(); });
     }
 
     public void UpdateAnimator()
